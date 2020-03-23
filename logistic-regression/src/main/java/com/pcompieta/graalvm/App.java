8518534892f419ee734b91fd195c0fdeb420bea6
Paolo Compieta <paolo.compieta@accenture.com>
2020-03-23 01:32:36 +0100

    better example of variable bindings for R+Py

--- logistic-regression/src/main/java/com/pcompieta/graalvm/App.java
+++ logistic-regression/src/main/java/com/pcompieta/graalvm/App.java
@@ -2,18 +2,30 @@ package com.pcompieta.graalvm;
 
 import org.graalvm.polyglot.*;
 
+/**
+ * See https://www.graalvm.org/docs/reference-manual/embed/
+ */
 class App {
     public static void main(String[] args) {
         javaR();
         javaPy();
     }
 
-    private static final int x = 42;
+    public static class Input {
+        public Input(int num, String text) {
+            this.num = num;
+            this.text = text;
+        }
+        public int num;
+        public String text;
+    }
+
+    private static final Input aGoodInput = new Input(42, "ciao");
 
     private static void javaR(){
         try (Context context = Context.newBuilder().allowAllAccess(true).build()) {
-            context.getBindings("R").putMember("x", x);
-            Value array = context.eval("R", "c(1,2,x,4)");
+            context.getBindings("R").putMember("x", aGoodInput);
+            Value array = context.eval("R", "c(1,2,x$num,4)");
             int result = array.getArrayElement(2).asInt();
             System.out.println("Java executing R: " + result);
         }
@@ -21,13 +33,13 @@ class App {
 
     private static void javaPy() {
         try (Context context = Context.newBuilder().allowIO(true).allowAllAccess(true).build()) {
-            context.getPolyglotBindings().putMember("x", x);
-            Value array = context.eval("python", "" +
+            context.getPolyglotBindings().putMember("x", aGoodInput);
+            Value v = context.eval("python", "" +
                 "import polyglot \n" +
                 "x = polyglot.import_value('x') \n" +
-                "[1,2,x,4]");
-            int result = array.getArrayElement(2).asInt();
-            System.out.println("Java executing Python: " + result);
+                "x['text']");
+            String returned = v.asString();
+            System.out.println("Java executing Python: " + returned);
         }
     }
 
